34aafdbb80124f33217a3c444ca6479d6fc4c6d3
Thomas Royer <cydonia572@gmail.com>
2023-03-06 14:36:09 +0100

    Add git and chezmoi functions

--- dot_bash.d/git.sh
+++ dot_bash.d/git.sh
@@ -9,19 +9,28 @@ git_clone() {
   cd $PROJECT
 }
 
-gcm() {
-  git commit -m $@
+gr() {
+  git restore --staged "$@" && git status
+}
+
+ga() {
+  git add "$@" && git status
 }
 
 alias g="git"
 alias ghc="git_clone github.com"
 alias glc="git_clone gitlab.com"
 alias gs="git status"
-alias ga="git add"
+alias gap="git add -p && git status"
+alias gam="git add -u && git status"
+alias gaa="git add -A && git status"
 alias gd="git diff"
 alias gdc="git diff --cached"
 alias gpl="git pull"
 alias gps="git push"
 alias gb="git branch"
 alias gc="git checkout"
+alias gnb="git checkout -b"
+alias gcm="git commit -m"
+alias gra="gr ."
 
