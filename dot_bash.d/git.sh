d2775aaf19f6480a1ba42ac20e7534e36f0fc72c
Thomas Royer <cydonia572@gmail.com>
2023-03-06 14:49:21 +0100

    Add alias for git logs

--- dot_bash.d/git.sh
+++ dot_bash.d/git.sh
@@ -35,4 +35,6 @@ alias gc="git checkout"
 alias gnb="git checkout -b"
 alias gcm="git commit -m"
 alias gra="gr ."
+alias gsh="git show"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
