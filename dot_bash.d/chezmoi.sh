57f4f425f3f59f34464b8aec70a9dbde30e0a3c9
Thomas Royer <cydonia572@gmail.com>
2023-03-06 14:42:27 +0100

    Add non interactive mode for chezmoi edit

--- dot_bash.d/chezmoi.sh
+++ dot_bash.d/chezmoi.sh
@@ -4,7 +4,11 @@ alias car="chezmoi apply -R && source $HOME/.bashrc"
 
 ce()
 {
-  file=$(chezmoi list --path-style=absolute | fzf --height 40% --reverse --border --prompt="Select the file you want to edit:")
+  if [ $# -eq 0 ]; then
+    file=$(chezmoi list --path-style=absolute | fzf --height 40% --reverse --border --prompt="Select the file you want to edit: ")
+  else
+    file=$(chezmoi list --path-style=absolute | fzf -q "$@" -1 -0 --height 40% --reverse --border --prompt="Select the file you want to edit: ")
+  fi
   handle_error "No file selected"
   chezmoi edit $file -a
   source $HOME/.bashrc
