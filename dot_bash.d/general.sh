46b1d62ff2d211bb3973b60e96a8dad017436a11
Thomas Royer <cydonia572@gmail.com>
2023-09-10 20:42:01 +0200

    Add aliases

--- dot_bash.d/general.sh
+++ dot_bash.d/general.sh
@@ -4,6 +4,6 @@ eval "$(starship init bash)"
 
 source /usr/share/fzf/key-bindings.bash
 
-alias sa="ssh-add"
-alias sl="keychain -l"
+alias sa="ssh-add" # Add key to the SSH agent
+alias sl="keychain -l" # List keys in the SSH agent
 
--- dot_bash.d/php.sh
+++ dot_bash.d/php.sh
@@ -1,8 +1,8 @@
-alias p="php"
-alias c="composer"
+alias p="php" # Alias for PHP
+alias c="composer" # Alias for Composer
 
-alias ci="composer install"
+alias ci="composer install" # Install composer packages
 
-alias su="symfony server:start -d"
-alias sd="symfony server:stop"
+alias su="symfony server:start -d" # Start symfony server
+alias sd="symfony server:stop" # Stop symfony server
 
