d800957e44c41b6cb2fc8cd19169f2c6d2713886
Thomas Royer <cydonia572@gmail.com>
2023-09-12 13:55:23 +0200

    Migrate from exa to eza

---
+++ dot_bash.d/eza.sh
@@ -0,0 +1,7 @@
+alias l='eza -lbF --git' # List current directory
+alias ls='eza' # List current directory without styling
+alias la='l -a' # List files including hidden
+
+alias lt='eza --tree --level=2' # List files recursively on 2 levels
+alias lp='lt $HOME/Projects' # List projects
+
