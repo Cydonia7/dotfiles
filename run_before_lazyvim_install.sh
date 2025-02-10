c73a4527217357d8f23e4ba12ba82f44e0d094cb
Thomas Royer <cydonia572@gmail.com>
2025-02-10 11:35:04 +0100

    Install Lazyvim properly

---
+++ run_before_lazyvim_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -d "$HOME/.config/nvim" ]; then
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+fi
+
+cd ~/.local/share/chezmoi/dot_config/nvim/lua/plugins
+for f in *; do
+  ln -sf ~/.local/share/chezmoi/dot_config/nvim/lua/plugins/$f ~/.config/nvim/lua/plugins/$f
+done
+rm -f ~/.config/nvim/lua/plugins/example.lua
