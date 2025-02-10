1bb3367f0df960dbd276641b16d3cb7c9c80d6b2
Thomas Royer <thomas.royer@cats-and-code.com>
2025-02-10 20:12:40 +0100

    Add fzf config

--- dot_bash.d/general.sh
+++ dot_bash.d/general.sh
@@ -1,9 +1,19 @@
-eval "$(zoxide init bash --hook prompt)"
 eval "$(keychain --eval -q)"
 eval "$(starship init bash)"
 
 source /usr/share/fzf/key-bindings.bash
 
-alias sa="ssh-add" # Add key to the SSH agent
+alias sa="ssh-add"     # Add key to the SSH agent
 alias sl="keychain -l" # List keys in the SSH agent
 
+eval "$(zoxide init bash)"
+
+eval -- "$(/usr/bin/starship init bash --print-full-init)"
+eval $(fzf --bash)
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
+source /usr/share/fzf/key-bindings.bash
