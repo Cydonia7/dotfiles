d3768f744848c0549b43f2dabea5dfc2406b99c4
Thomas Royer <thomas.royer@cats-and-code.com>
2025-02-17 10:38:34 +0100

    Yay sudoloop

--- dot_bash.d/yay.sh
+++ dot_bash.d/yay.sh
@@ -1,4 +1,5 @@
-alias yi="yay -S" # Install package
-alias yr="yay -Rscn" # Remove package
-alias yu="yay -Syu" # Update packages
+alias yay="yay --sudoloop"
+alias yi="yay -S"                # Install package
+alias yr="yay -Rscn"             # Remove package
+alias yu="yay -Syu"              # Update packages
 alias yun="yay -Syu --noconfirm" # Update packages without confirmation
