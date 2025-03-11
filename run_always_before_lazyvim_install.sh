15b445e05df773ea1c9689741458a7cb1f66ff15
Thomas Royer <thomas.royer@cats-and-code.com>
2025-03-11 15:41:07 +0100

    Add luasnip

--- run_always_before_lazyvim_install.sh
+++ run_always_before_lazyvim_install.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-if [ ! -d "$HOME/.config/nvim" ]; then
-  git clone https://github.com/LazyVim/starter ~/.config/nvim
+NVIM_CONFIG="$HOME/.config/nvim"
+CHEZMOI_CONFIG="$HOME/.local/share/chezmoi/dot_config/nvim"
+
+if [ ! -d "$NVIM_CONFIG" ]; then
+  git clone https://github.com/LazyVim/starter "$NVIM_CONFIG"
+  rm -f ~/.config/nvim/lua/plugins/example.lua
 fi
 
-cd ~/.local/share/chezmoi/dot_config/nvim/lua/plugins
-for f in *; do
-  ln -sf ~/.local/share/chezmoi/dot_config/nvim/lua/plugins/$f ~/.config/nvim/lua/plugins/$f
-done
-rm -f ~/.config/nvim/lua/plugins/example.lua
+find "$CHEZMOI_CONFIG" -type d | while read -r src_dir; do
+  dest_dir="$NVIM_CONFIG${src_dir#$CHEZMOI_CONFIG}"
+  mkdir -p "$dest_dir"
 
-cd ~/.local/share/chezmoi/dot_config/nvim/lua/config
-for f in *; do
-  ln -sf ~/.local/share/chezmoi/dot_config/nvim/lua/config/$f ~/.config/nvim/lua/config/$f
+  for file in "$src_dir"/*; do
+    if [ -f "$file" ]; then
+      ln -sf "$file" "$dest_dir/$(basename "$file")"
+    fi
+  done
 done
