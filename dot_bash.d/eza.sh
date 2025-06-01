4ab3531ead9b207c0f98edee3f40d0e343098713
Thomas Royer <thomas.royer@cats-and-code.com>
2025-06-01 17:40:17 +0200

    Eza levels

--- dot_bash.d/eza.sh
+++ dot_bash.d/eza.sh
@@ -1,7 +1,11 @@
 alias l='eza -lbF --git' # List current directory
-alias ls='eza' # List current directory without styling
-alias la='l -a' # List files including hidden
+alias ls='eza'           # List current directory without styling
+alias la='l -a'          # List files including hidden
 
-alias lt='eza --tree --level=2' # List files recursively on 2 levels
+alias lt='eza --tree' # List files recursively
+
+for i in {1..5}; do
+  alias lt$i="lt --level=$i"
+done
+
 alias lp='lt $HOME/Projects' # List projects
-
