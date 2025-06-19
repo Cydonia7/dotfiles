364e98b80d61de5787deb4a089f840bf9ed30173
Thomas Royer <thomas.royer@cats-and-code.com>
2025-06-19 08:28:05 +0200

    Add polybar click on cal

---
+++ dot_bin/center.sh
@@ -0,0 +1,13 @@
+vcenter() {
+  local cols=$(tput cols)
+  local rows=$(tput lines)
+
+  awk -v cols="$cols" -v rows="$rows" '
+        { buf[NR] = $0 }                      # stash lines
+        END {
+            vpad = int((rows - NR) / 2)       # blank lines above
+            for (i = 0; i < vpad; ++i) print ""
+            for (i = 1; i <= NR; ++i)
+                printf "%*s\n", int((cols + length(buf[i])) / 2), buf[i]
+        }'
+}
