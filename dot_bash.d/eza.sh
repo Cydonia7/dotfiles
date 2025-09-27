b4b12b11b719bc4ece3b6a230bb2c1e27106acff
Thomas Royer <thomas.royer@cats-and-code.com>
2025-09-27 21:38:11 +0200

    Eza icons

--- dot_bash.d/eza.sh
+++ dot_bash.d/eza.sh
@@ -1,6 +1,6 @@
-alias l='eza -lbF --git' # List current directory
-alias ls='eza'           # List current directory without styling
-alias la='l -a'          # List files including hidden
+alias l='eza -lbF --git --icons' # List current directory
+alias ls='eza --icons'           # List current directory without styling
+alias la='l -a'                  # List files including hidden
 
 alias lt='eza --tree' # List files recursively
 
